781426661bc23ad1c92fb5fdcd75a3af179df148
John Pak <pakman1126@yahoo.com>
2018-04-18 12:02:58 -0700

    4.5 Views

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -5,4 +5,8 @@ class SimplePagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
   end
+
+  def landing_page
+    @products = Product.limit(3)
+  end
 end
