5c5e013fffbf11829cf3b9b998488faed43eede2
John Pak <pakman1126@yahoo.com>
2018-04-20 11:59:07 -0700

    4.6 Orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!")
+Order.create(user_id: 1, product_id: 3, total: 10.0)
+Order.create(user_id: 1, product_id: 4, total: 5.0)
