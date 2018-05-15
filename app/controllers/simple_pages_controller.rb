f6d9134c170abbbcc254d53c0ef979232d850281
John Pak <pakman1126@yahoo.com>
2018-05-15 10:11:43 -0700

    5.3 Sendgrid

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -10,3 +10,11 @@ class SimplePagesController < ApplicationController
     @products = Product.limit(3)
   end
 end
+
+ActionMailer::Base.mail(from: 'from@domain.com', to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email, to: 'pakman1126@yahoo.com', subject: "A new contact form message from #{@name}", body: @message).deliver_now
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
 
   get 'simple_pages/landing_page'
+
   root 'simple_pages#landing_page'
+
+  post 'simple_pages/thank_you'
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
