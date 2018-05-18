174990d99e74e5af65d5e0ff9319111a047a7d55
John Pak <pakman1126@yahoo.com>
2018-05-18 09:17:53 -0700

    update email

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
   def contact_form
-    UserMailer.contact_form("pakman1126@yahoo.com", "John", "Hello There!")
+    UserMailer.contact_form("John@sample.com", "John", "Hello World!")
   end
 end
