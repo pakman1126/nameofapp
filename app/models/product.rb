c56f38e9ee8f90735ca11e94f9f9dcf8c5f6dda1
John Pak <pakman1126@yahoo.com>
2018-05-18 08:10:50 -0700

    5.2 correct mailer and case sensitive

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,9 @@ class Product < ApplicationRecord
   has_many :orders
 
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    if Rails.env != "production"
+      Product.where("name || color || description LIKE ?", "%#{search_term}%")
+    else
+      Product.where("name || color || description iLIKE ?", "%#{search_term}%")
   end
 end
