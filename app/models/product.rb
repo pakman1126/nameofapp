db791dbd90a1a4266d9eb4e57799109b480e8a5a
John Pak <pakman1126@yahoo.com>
2018-06-01 12:01:21 -0700

    5.7 The Comment Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
+  has_many :comments
 
   def self.search(search_term)
     if Rails.env != "production"
@@ -7,5 +8,13 @@ class Product < ApplicationRecord
     else
       Product.where("name || color || description iLIKE ?", "%#{search_term}%")
     end
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def lowest_rating_comment
+    comments.rating_asc.first
   end
 end
