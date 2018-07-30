8a5d2af03d5e11ffc0a17defd63734fa5d2f10c6
John Pak <pakman1126@yahoo.com>
2018-07-30 11:11:30 -0500

    make corrections

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,7 +18,8 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @comments = @product.comments.order("created_at DESC")
-    @comments = Comment.paginate(page: params[:page], per_page: 2)
+    @comments = @product.comments.paginate(page: params[:page], per_page: 2)
+    @product.viewed
   end
 
   # GET /products/new
