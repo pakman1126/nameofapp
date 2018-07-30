602840f1029866df82dfd844d293dda865462992
John Pak <pakman1126@yahoo.com>
2018-07-29 20:38:18 -0500

    Use Redis for view counter

--- app/models/product.rb
+++ app/models/product.rb
@@ -23,4 +23,20 @@ class Product < ApplicationRecord
   def average_rating
     comments.average(:rating).to_f
   end
+
+  def views
+  	$redis.get("product:#{id}")
+  end
+
+  def viewed
+  	$redis.incr("product:#{id}")
+  end
+
+  def set_recent_reviewer(user_name)
+  	$redis.set("rr_product:#{id}","#{user_name}")
+  end
+
+  def get_recent_reviewer
+  	$redis.get("rr_product:#{id}")
+  end
 end
